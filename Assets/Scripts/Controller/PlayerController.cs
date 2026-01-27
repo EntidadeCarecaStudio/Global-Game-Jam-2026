2aec3311e6eacd7cad33649af1b15d5d9ea5c2ee
Everton Borges <evertonsoaresborges@gmail.com>
2026-01-27 11:09:09 -0400

    feat: player controller with animations

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,49 +4,43 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    
-    private enum PlayerState
-    {
-        Idle,
-        Run,
-        Attack,
-        Dodge,
-        TakeDamage,
-        Die
-    }
-
-    [SerializeField] private float _movementSpeed = 5.0f;
+    [SerializeField] private float _movementSpeedX = 5.0f;
+    [SerializeField] private float _movementSpeedZ = 3.0f;
     [SerializeField] private float _dodgeSpeedMultiplier = 2.0f;
     [SerializeField] private float _dodgeDuration = 0.5f;
     [SerializeField] private float _attackDuration = 0.4f;
     [SerializeField] private int _maxHealth = 100;
-    [SerializeField] private Animator _animator;
+    [SerializeField] private CharacterAnimationController _characterAnimationController;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     private int m_currentHealth;
-    private PlayerState m_currentState;
+    private CharacterState m_currentState;
     private Vector3 m_movementInput;
+    private Vector3 m_dodgeDirection;
     private float m_stateTimer;
     private bool m_canPerformAction = true;
     private Rigidbody m_rigidbody;
-    private Vector3 m_facingDirection = Vector3.right;
+    private Vector3 m_facingDirection = Vector3.back;
 
     public int CurrentHealth => m_currentHealth;
 
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody.freezeRotation = true;
     }
 
     private void Start()
     {
         m_currentHealth = _maxHealth;
-        SetState(PlayerState.Idle);
+        SetState(CharacterState.Idle);
     }
 
     private void Update()
     {
         HandleInput();
         UpdateStateLogic();
+        UpdateSpriteOrientation();
     }
 
     private void FixedUpdate()
@@ -54,37 +48,44 @@ public class PlayerController : MonoBehaviour
         PerformMovement(m_movementInput);
     }
 
-    private void SetState(PlayerState newState)
+    private void SetState(CharacterState newState)
     {
-        m_currentState = newState;
-        m_stateTimer = 0f;
-        UpdateAnimator();
+        if (m_currentState != newState)
+        {
+            m_currentState = newState;
+            m_stateTimer = 0f;
+            if (_characterAnimationController != null)
+            {
+                _characterAnimationController.UpdateAnimation(m_currentState);
+            }
+        }
     }
 
     private void HandleInput()
     {
-        if (m_currentState == PlayerState.Die)
+        if (m_currentState == CharacterState.Die)
         {
             m_movementInput = Vector3.zero;
             return;
         }
 
-        float horizontalInput = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ? -1f :
-                                Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ? 1f : 
-                                0f;
-        float verticalInput = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ? -1f :
-                              Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ? 1f : 
-                              0f;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
         m_movementInput = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
+        if (m_movementInput.x != 0)
+        {
+            m_facingDirection = new Vector3(m_movementInput.x, 0f, 0f).normalized;
+        }
+
         if (m_canPerformAction)
         {
-            if (Input.GetKey(KeyCode.Z))
+            if (Input.GetButton("Attack"))
             {
                 Attack();
             }
-            else if (Input.GetKey(KeyCode.Space))
+            else if (Input.GetButton("Dodge"))
             {
                 Dodge();
             }
@@ -97,110 +98,157 @@ public class PlayerController : MonoBehaviour
 
         switch (m_currentState)
         {
-            case PlayerState.Idle:
-            case PlayerState.Run:
-                if (m_movementInput != Vector3.zero)
-                {
-                    SetState(PlayerState.Run);
-                    m_facingDirection = m_movementInput;
-                }
-                else
-                {
-                    SetState(PlayerState.Idle);
-                }
+            case CharacterState.Idle:
+                HandleIdleStateLogic();
                 break;
-
-            case PlayerState.Attack:
-                if (m_stateTimer >= _attackDuration)
-                {
-                    m_canPerformAction = true;
-                    if (m_movementInput != Vector3.zero)
-                    {
-                        SetState(PlayerState.Run);
-                    }
-                    else
-                    {
-                        SetState(PlayerState.Idle);
-                    }
-                }
+            case CharacterState.Run:
+                HandleRunStateLogic();
                 break;
-
-            case PlayerState.Dodge:
-                if (m_stateTimer >= _dodgeDuration)
-                {
-                    m_canPerformAction = true;
-                    if (m_movementInput != Vector3.zero)
-                    {
-                        SetState(PlayerState.Run);
-                    }
-                    else
-                    {
-                        SetState(PlayerState.Idle);
-                    }
-                }
-                PerformDodgeMovement();
+            case CharacterState.Attack:
+                HandleAttackStateLogic();
                 break;
-
-            case PlayerState.TakeDamage:
-                if (m_stateTimer >= 0.3f)
-                {
-                    if (m_currentHealth <= 0)
-                    {
-                        Die();
-                    }
-                    else
-                    {
-                        SetState(PlayerState.Idle);
-                    }
-                }
+            case CharacterState.Dodge:
+                HandleDodgeStateLogic();
                 break;
-
-            case PlayerState.Die:
+            case CharacterState.TakeDamage:
+                HandleTakeDamageStateLogic();
+                break;
+            case CharacterState.Die:
+                HandleDieStateLogic();
                 break;
         }
     }
 
+    private void HandleIdleStateLogic()
+    {
+        if (m_movementInput != Vector3.zero)
+        {
+            SetState(CharacterState.Run);
+        }
+    }
+
+    private void HandleRunStateLogic()
+    {
+        if (m_movementInput == Vector3.zero)
+        {
+            SetState(CharacterState.Idle);
+        }
+    }
+
+    private void HandleAttackStateLogic()
+    {
+        if (m_stateTimer >= _attackDuration)
+        {
+            m_canPerformAction = true;
+            if (m_movementInput != Vector3.zero)
+            {
+                SetState(CharacterState.Run);
+            }
+            else
+            {
+                SetState(CharacterState.Idle);
+            }
+        }
+    }
+
+    private void HandleDodgeStateLogic()
+    {
+        if (m_stateTimer >= _dodgeDuration)
+        {
+            m_canPerformAction = true;
+            if (m_movementInput != Vector3.zero)
+            {
+                SetState(CharacterState.Run);
+            }
+            else
+            {
+                SetState(CharacterState.Idle);
+            }
+        }
+        PerformDodgeMovement();
+    }
+
+    private void HandleTakeDamageStateLogic()
+    {
+        if (m_stateTimer >= 0.3f)
+        {
+            if (m_currentHealth <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                SetState(CharacterState.Idle);
+            }
+        }
+    }
+
+    private void HandleDieStateLogic()
+    {
+
+    }
+
     private void PerformMovement(Vector3 direction)
     {
-        if (m_currentState == PlayerState.Dodge) return;
-        if (m_currentState == PlayerState.Attack ||
-            m_currentState == PlayerState.TakeDamage ||
-            m_currentState == PlayerState.Die)
+        if (m_currentState == CharacterState.Dodge)
+        {
+            return;
+        }
+        if (m_currentState == CharacterState.Attack ||
+            m_currentState == CharacterState.TakeDamage ||
+            m_currentState == CharacterState.Die)
         {
             m_rigidbody.linearVelocity = Vector3.zero;
             return;
         }
 
-        m_rigidbody.linearVelocity = direction * _movementSpeed;
+        Vector3 currentVelocity = m_rigidbody.linearVelocity;
+
+        currentVelocity.x = direction.x * _movementSpeedX;
+        currentVelocity.z = direction.z * _movementSpeedZ;
+
+        m_rigidbody.linearVelocity = currentVelocity;
     }
 
     private void PerformDodgeMovement()
     {
-        m_rigidbody.linearVelocity = m_facingDirection * _movementSpeed * _dodgeSpeedMultiplier;
+        Vector3 currentVelocity = m_rigidbody.linearVelocity;
+        currentVelocity.x = m_dodgeDirection.x * _movementSpeedX * _dodgeSpeedMultiplier;
+        currentVelocity.z = m_dodgeDirection.z * _movementSpeedZ * _dodgeSpeedMultiplier;
+        m_rigidbody.linearVelocity = currentVelocity;
     }
 
     private void Attack()
     {
-        if (m_currentState == PlayerState.Attack || m_currentState == PlayerState.Dodge ||
-            m_currentState == PlayerState.TakeDamage || m_currentState == PlayerState.Die) return;
+        if (m_currentState == CharacterState.Attack || m_currentState == CharacterState.Dodge ||
+            m_currentState == CharacterState.TakeDamage || m_currentState == CharacterState.Die) return;
 
-        SetState(PlayerState.Attack);
+        SetState(CharacterState.Attack);
         m_canPerformAction = false;
     }
 
     private void Dodge()
     {
-        if (m_currentState == PlayerState.Attack || m_currentState == PlayerState.Dodge ||
-            m_currentState == PlayerState.TakeDamage || m_currentState == PlayerState.Die) return;
+        if (m_currentState == CharacterState.Attack || m_currentState == CharacterState.Dodge ||
+            m_currentState == CharacterState.TakeDamage || m_currentState == CharacterState.Die) return;
 
-        SetState(PlayerState.Dodge);
+        SetState(CharacterState.Dodge);
         m_canPerformAction = false;
+
+        if (m_movementInput != Vector3.zero)
+        {
+            m_dodgeDirection = m_movementInput;
+        }
+        else
+        {
+            m_dodgeDirection = m_facingDirection;
+        }
     }
 
     public void TakeDamage(int damage)
     {
-        if (m_currentState == PlayerState.Die) return;
-        if (m_currentState == PlayerState.Dodge) return;
+        if (m_currentState == CharacterState.Die) return;
+        if (m_currentState == CharacterState.Dodge) return;
 
         m_currentHealth -= damage;
         if (m_currentHealth <= 0)
@@ -210,78 +258,41 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            SetState(PlayerState.TakeDamage);
+            SetState(CharacterState.TakeDamage);
             StartCoroutine(DamageFeedback());
         }
     }
 
     private void Die()
     {
-        SetState(PlayerState.Die);
+        SetState(CharacterState.Die);
         Debug.Log("Player has died!");
         enabled = false;
     }
 
-    private void UpdateAnimator()
+    private void UpdateSpriteOrientation()
     {
-        if (_animator == null) return;
+        if (_spriteRenderer == null) return;
 
-        _animator.ResetTrigger("Idle");
-        _animator.ResetTrigger("Run");
-        _animator.ResetTrigger("Attack");
-        _animator.ResetTrigger("Dodge");
-        _animator.ResetTrigger("TakeDamage");
-        _animator.ResetTrigger("Die");
-
-        switch (m_currentState)
+        if (m_facingDirection.x < 0)
         {
-            case PlayerState.Idle:
-                _animator.SetBool("IsRunning", false);
-                break;
-            case PlayerState.Run:
-                _animator.SetBool("IsRunning", true);
-                _animator.SetFloat("MoveX", m_movementInput.x);
-                _animator.SetFloat("MoveY", m_movementInput.y);
-                break;
-            case PlayerState.Attack:
-                _animator.SetBool("IsRunning", false);
-                _animator.SetTrigger("Attack");
-                break;
-            case PlayerState.Dodge:
-                _animator.SetBool("IsRunning", false);
-                _animator.SetTrigger("Dodge");
-                break;
-            case PlayerState.TakeDamage:
-                _animator.SetBool("IsRunning", false);
-                _animator.SetTrigger("TakeDamage");
-                break;
-            case PlayerState.Die:
-                _animator.SetBool("IsRunning", false);
-                _animator.SetTrigger("Die");
-                break;
+            _spriteRenderer.flipX = true;
         }
-
-        if (m_movementInput != Vector3.zero)
+        else if (m_facingDirection.x > 0)
         {
-             _animator.SetFloat("LastMoveX", m_movementInput.x);
-             _animator.SetFloat("LastMoveY", m_movementInput.y);
-        }
-        else if (m_currentState == PlayerState.Idle)
-        {
-            _animator.SetFloat("LastMoveX", m_facingDirection.x);
-            _animator.SetFloat("LastMoveY", m_facingDirection.y);
+            _spriteRenderer.flipX = false;
         }
     }
 
     private IEnumerator DamageFeedback()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null)
+        if (_spriteRenderer != null)
         {
-            Color originalColor = spriteRenderer.color;
-            spriteRenderer.color = Color.red;
+            Color originalColor = _spriteRenderer.color;
+            _spriteRenderer.color = Color.red;
             yield return new WaitForSeconds(0.1f);
-            spriteRenderer.color = originalColor;
+            _spriteRenderer.color = originalColor;
         }
     }
+
 }
