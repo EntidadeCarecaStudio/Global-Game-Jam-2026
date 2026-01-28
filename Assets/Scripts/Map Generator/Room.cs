3c8afd13382c918ec79be9312b37e20f23ed0143
ChoCareca <entidadecarecastudio@gmail.com>
2026-01-28 16:54:56 -0300

    Spawn de inimigos nas salas

--- Assets/Scripts/Map Generator/Room.cs
+++ Assets/Scripts/Map Generator/Room.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 
 public class Room : MonoBehaviour
 {
-    public enum RoomType { Room, Corridor, Boss, Start }
+    // Adicionado o tipo MiniBoss
+    public enum RoomType { Room, Corridor, Boss, Start, MiniBoss }
     public RoomType type;
+
+    // Novo enum para definir o elemento (importante para os Minibosses)
+    public enum RoomElement { None, Fire, Ice, Earth }
+    public RoomElement element; // Defina isso no Inspector dos prefabs de Miniboss
+    
+    [Header("Connections")]
     public List<Transform> connectors;
+
+    [Header("Spawning")]
+    public List<Transform> spawnPoints; 
+
+    void OnDrawGizmos()
+    {
+        if (spawnPoints != null)
+        {
+            Gizmos.color = Color.red;
+            foreach (var sp in spawnPoints)
+            {
+                if(sp != null) Gizmos.DrawSphere(sp.position, 0.5f);
+            }
+        }
+    }
 }
